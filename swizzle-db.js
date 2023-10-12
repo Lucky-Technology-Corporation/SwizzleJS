77b0c8bcb51aced5473d6137b91c8211c3f0e2fb
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2023-10-12 16:01:28 -0700

    v 26

--- swizzle-db.js
+++ swizzle-db.js
@@ -2,6 +2,15 @@ const {connectDB} = require('./swizzle-db-connection');
 
 let _db;
 
+const init = async () => {
+  try {
+    _db = await connectDB();
+  } catch (err) {
+    console.error('Failed to connect to DB:', err);
+  }
+};
+
+
 Object.defineProperty(module.exports, 'db', {
   get: () => {
     if (!_db) {
@@ -14,3 +23,5 @@ Object.defineProperty(module.exports, 'db', {
     return _db;
   }
 });
+
+module.exports.init = init;
